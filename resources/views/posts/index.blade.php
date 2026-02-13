ca630e704182fdd336f6beebc100ac2390e090d3
Sophie-Anne Lopez <130466238+Onyyx256@users.noreply.github.com>
2026-02-13 16:31:09 -0500

    feat: added post button when no posts are already there

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,10 +1,14 @@
 <x-layout>
     <h1>Posts List</h1>
-    <ul>
-        @foreach($posts as $post)
-            <li>
-                <a href="/posts/{{ $post -> id }}">{{ $post -> title }}</a>
-            </li>
-        @endforeach
-    </ul>
+    @if(count($posts) > 0)
+        <ul>
+            @foreach($posts as $post)
+                <li>
+                    <a href="/posts/{{ $post -> id }}">{{ $post -> title }}</a>
+                </li>
+            @endforeach
+        </ul>
+    @else
+        <p>So empty... <a href="/posts/create">wanna try to post something?</a></p>
+    @endif
 </x-layout>
